866342a0c0db51ea586494e45f46416727c97eb1
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-10-20 20:50:29 +0200

    policy iteration should be ok. Add some doc and clean a bit the code

--- assignment1/policy_iteration/student.py
+++ assignment1/policy_iteration/student.py
@@ -1,26 +1,32 @@
 import numpy as np
 
+
 def reward_function(s, env_size):
-    #print("ENV_SIZE: " + str(env_size))
-    #print("STATE in reward_function is: "  + str(s))
+    
     if (s == [env_size-1,env_size-1]).all():
         return 1
     return 0
-
-def transition_function(s, a, directions,env_size,holes):
-        s_prime = s + directions[a,:]
-
-        if s_prime[0] < env_size and s_prime[1] < env_size and (s_prime >= 0).all():
-            if holes[s_prime[0], s_prime[1]] == 0:
-                return s_prime
-
-        return s
+def is_state_valid(s_prime,env_size,holes):
+    if s_prime[0] < env_size and s_prime[1] < env_size and (s_prime >= 0).all():
+        if holes[s_prime[0], s_prime[1]] == 0 :
+            return True
+    return False
 
 def transition_probabilities(env, s, a, env_size, directions, holes):
 
-    cells = []
-    probs = []
     prob_next_state = np.zeros((env_size, env_size))
-    s_prime = transition_function(s, a, directions,env_size,holes)
-    prob_next_state[s_prime[0],s_prime[1]] =  1.0
+    
+    s_prime_correct = s + directions[a]
+    if (is_state_valid(s_prime_correct,env_size,holes)):
+        prob_next_state[s_prime_correct[0],s_prime_correct[1]] =  0.5
+    
+    if(a==0):
+        s_prime_random = s + directions[len(directions)-1]
+    else:
+        s_prime_random = s + directions[a-1]
+    
+    if (is_state_valid(s_prime_random,env_size,holes)):
+        prob_next_state[s_prime_random[0],s_prime_random[1]] =  0.5
+           
+
     return prob_next_state
