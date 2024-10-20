1f7993de6390f3c6a34cd4923845a29238967037
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-10-20 18:08:51 +0200

    Reward function might be ok, game starts

--- assignment1/policy_iteration/student.py
+++ assignment1/policy_iteration/student.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 def reward_function(s, env_size):
-    r = ...
-    return r
+    #print("ENV_SIZE: " + str(env_size))
+    #print("STATE in reward_function is: "  + str(s))
+    if (s == [env_size,env_size]).all():
+        return 1
+    return 0
 
 def transition_probabilities(env, s, a, env_size, directions, holes):
     cells = []
     probs = []
     prob_next_state = np.zeros((env_size, env_size))
 
-    prob_next_state = ...
+    #prob_next_state =  #TODO
 
     return prob_next_state
