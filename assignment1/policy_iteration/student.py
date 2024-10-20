0560de1ac5a51e3bfcd2846424b26b43eb53964e
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-10-20 19:22:44 +0200

    Deterministic transition_probabilities working, switching to non deterministic

--- assignment1/policy_iteration/student.py
+++ assignment1/policy_iteration/student.py
@@ -3,7 +3,7 @@ import numpy as np
 def reward_function(s, env_size):
     #print("ENV_SIZE: " + str(env_size))
     #print("STATE in reward_function is: "  + str(s))
-    if (s == [env_size,env_size]).all():
+    if (s == [env_size-1,env_size-1]).all():
         return 1
     return 0
 
@@ -11,18 +11,16 @@ def transition_function(s, a, directions,env_size,holes):
         s_prime = s + directions[a,:]
 
         if s_prime[0] < env_size and s_prime[1] < env_size and (s_prime >= 0).all():
-            if holes[s_prime[0], s_prime[1]] == 0 :
+            if holes[s_prime[0], s_prime[1]] == 0:
                 return s_prime
 
         return s
 
 def transition_probabilities(env, s, a, env_size, directions, holes):
+
     cells = []
     probs = []
     prob_next_state = np.zeros((env_size, env_size))
-     
     s_prime = transition_function(s, a, directions,env_size,holes)
-   
-    prob_next_state[s_prime[0],s_prime[1]] =  1
-    
+    prob_next_state[s_prime[0],s_prime[1]] =  1.0
     return prob_next_state
