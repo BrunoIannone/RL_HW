f34865386a22bc9470e4204802391bac4e4b8636
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-10-20 18:52:50 +0200

    Deterministic transition function not working

--- assignment1/policy_iteration/student.py
+++ assignment1/policy_iteration/student.py
@@ -7,11 +7,22 @@ def reward_function(s, env_size):
         return 1
     return 0
 
+def transition_function(s, a, directions,env_size,holes):
+        s_prime = s + directions[a,:]
+
+        if s_prime[0] < env_size and s_prime[1] < env_size and (s_prime >= 0).all():
+            if holes[s_prime[0], s_prime[1]] == 0 :
+                return s_prime
+
+        return s
+
 def transition_probabilities(env, s, a, env_size, directions, holes):
     cells = []
     probs = []
     prob_next_state = np.zeros((env_size, env_size))
-
-    #prob_next_state =  #TODO
-
+     
+    s_prime = transition_function(s, a, directions,env_size,holes)
+   
+    prob_next_state[s_prime[0],s_prime[1]] =  1
+    
     return prob_next_state
