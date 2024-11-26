7e57b16b80cb99542e3e79d9eeaba068eb2e4077
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-11-26 21:59:13 +0100

    Push a caso

--- assignment3/car_racing/student.py
+++ assignment3/car_racing/student.py
@@ -11,8 +11,20 @@ class Policy(nn.Module):
         super(Policy, self).__init__()
         self.device = device
 
-    def forward(self, x):
-        # TODO
+        #### PARAMS ####
+        self.batch = 10 #minibatch k
+        self.step_size = 0.1 # eta (n greca)
+        self.replay_period = 10 # K (capital k)
+        self.size = 10 # N
+        self.alpha = 1
+        self.beta = 1
+        self.budget = 10 # T
+        self.replay_memory = [] # H
+        self.delta = 0
+        self.p1 = 1
+
+    def forward(self, x): # TODO
+        
         return x
     
     def act(self, state):
@@ -20,8 +32,11 @@ class Policy(nn.Module):
         return 
 
     def train(self):
-        # TODO
-        return
+        env = gym.make('CarRacing-v3', continuous=True)
+        print(env.sample())
+        for t in range(1,self.budget):
+            pass
+            #return
 
     def save(self):
         torch.save(self.state_dict(), 'model.pt')
